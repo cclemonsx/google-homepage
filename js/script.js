a07f73502be7ee5d22d06b42d468cbf8940c69b8
Cece Xue <cecileaxue@gmail.com>
2018-06-10 22:30:42 -0400

    added jquery to fix stuff even though not in css syllabus:)

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@
 
 //alert('loaded')
 //every time you make new js script, this will make a little pop-up window, telling you it's linked correctly
+
+
+$(window).resize(function() {
+  let height=$(window).height()
+  if(height>587){
+    $("footer").css("position", "fixed")
+  }
+  else {
+    $("footer").css("position", "relative")
+  }
+  console.log(height)
+})
